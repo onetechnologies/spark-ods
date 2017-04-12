859d49c6ef30d3a523fb2d50e06529e3b59540e8
Masayoshi Kuroda <kuroda@toyo.co.jp>
2017-04-12 07:42:17 +0000

    Upload new file

---
+++ src/main/scala/jp/co/toyo/spark/ods/ODSRelationProvider.scala
@@ -0,0 +1,12 @@
+package jp.co.toyo.spark.ods
+
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.sources.{ RelationProvider, DataSourceRegister, BaseRelation }
+
+class ODSRelationProvider extends RelationProvider with DataSourceRegister {
+  override def shortName(): String = "ods"
+
+  override def createRelation(sqlContext: SQLContext, parameters: Map[String, String]): BaseRelation = {
+    return new ODSRelation(parameters)(sqlContext)
+  }
+}
